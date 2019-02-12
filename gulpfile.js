8eaa4cdbc75703b789f05e209f1d45986bfea9c0
Moshe <firstaidabcd@gmail.com>
2019-02-12 21:05:11 +0100

    mess around in CSS split it up in modules

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
+
+gulp.task('default',function(){
+    console.log("Hoera we hebben een gulp task");
+});
+
+gulp.task('html',function(){
+   console.log("Imagine something useful being done to your HTML here.");
+
+});
+
+gulp.task('styles',function(){
+   return gulp.src('./app/assets/styles/styles.css')
+   .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
+   .pipe(gulp.dest('./app/temp/styles/styles.css'));
+});
+
+gulp.task('watch', function(){
+    watch('./app/index.html', function(){
+    gulp.start('html');
+});
+    watch('./app/assets/styles/**/*.css',function(){
+    gulp.start('styles');
+});
+});
