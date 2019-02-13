9c3a313b64fefd184a02a59c001e60e5b059f8e4
Moshe <firstaidabcd@gmail.com>
2019-02-13 14:18:39 +0100

    BrowserSync installed in Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssImport = require('postcss-import');
+var browserSync = require('browser-sync').create();
 
 gulp.task('default',function(){
     console.log("Hoera we hebben een gulp task");
@@ -18,14 +19,24 @@ gulp.task('html',function(){
 gulp.task('styles',function(){
    return gulp.src('./app/assets/styles/styles.css')
    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-   .pipe(gulp.dest('./app/temp/styles/styles.css'));
+   .pipe(gulp.dest('./app/temp/styles/'));
 });
 
 gulp.task('watch', function(){
+  browserSync.init({
+  server: {
+    baseDir: "app"
+  }
+});
     watch('./app/index.html', function(){
-    gulp.start('html');
+    browserSync.reload();
 });
     watch('./app/assets/styles/**/*.css',function(){
-    gulp.start('styles');
+    gulp.start('cssInject');
 });
+});
+
+gulp.task('cssInject', ['styles'] ,function(){
+  return gulp.src('./app/temp/styles/styles.css')
+  .pipe(browserSync.stream());
 });
